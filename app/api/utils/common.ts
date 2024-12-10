30618c9f71be8bf5a3e8c4acf1332074b100efa2
马小雅 <maxiaoya@tuhu.cn>
2024-12-10 11:23:41 +0800

    feat: iframe部署版本，主文档通过路由给iframe传入appId，解决iframe页面无法携带cookie

--- app/api/utils/common.ts
+++ app/api/utils/common.ts
@@ -6,8 +6,18 @@ import { API_KEY, API_URL, APP_ID } from '@/config'
 // const userPrefix = `user_${APP_ID}:`
 
 export const getInfo = (request: NextRequest) => {
-  const sessionId = request.cookies.get('session_id')?.value || v4()
-  const app_id = request.cookies.get('APP_ID')?.value || 'default_app_id'
+  const headers = request.headers;
+  const referer = headers.get('referer') || 'unknown';
+  let app_id = 'default_app_id';
+  if (referer !== 'unknown') {
+    const refererUrl = new URL(referer);
+    app_id = refererUrl.searchParams.get('appId') || 'default_app_id';
+  }
+
+  // const sessionId = request.cookies.get('session_id')?.value || v4()
+  const sessionId = 'default_session_id';
+  // const app_id = request.cookies.get('APP_ID')?.value || 'default_app_id'
+
   const userPrefix = `user_${app_id}:`
   const user = userPrefix + sessionId
   return {
